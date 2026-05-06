6103d3de2fc34c73d0b5f950b1f1a915537fc69d
acharyarajan063 <acharyarajan063@gmail.com>
2026-05-06 10:06:50 +0200

    datbase

--- student_record_system.sql
+++ student_record_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 06, 2026 at 09:37 AM
+-- Generation Time: May 06, 2026 at 10:02 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
