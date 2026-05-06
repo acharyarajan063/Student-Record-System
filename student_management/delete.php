8ffcbc065ff0de737de846e7063d8fda5cac717a
acharyarajan063 <acharyarajan063@gmail.com>
2026-05-06 10:44:19 +0200

    delete function applied and worked correctly

---
+++ student_management/delete.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../controllers/StudentController.php';
+
+$controller = new StudentController();
+
+if (isset($_GET['id'])) {
+
+    $id = $_GET['id'];
+
+    $controller->destroy($id);
+
+    header("Location: index.php");
+    exit();
+}
+?>
