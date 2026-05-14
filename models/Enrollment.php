ffc2dff5f1387f0a21da7457c9fee3c780c1803a
acharyarajan063 <acharyarajan063@gmail.com>
2026-05-14 15:44:05 +0200

    created entollment.php and created all funcanality inside it such as: create delete etc.

---
+++ models/Enrollment.php
@@ -0,0 +1,82 @@
+<?php
+
+require_once __DIR__ . '/../database/db.php';
+
+class Enrollment
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $db = new Database();
+
+        $this->conn = $db->connect();
+    }
+
+    // Get All Enrollments
+    public function getAll()
+    {
+        $sql = "
+        SELECT
+            enrollment.EnrollmentID,
+            student.StudentName,
+            course.CourseName,
+            enrollment.EnrollDate
+        FROM enrollment
+        INNER JOIN student
+            ON enrollment.StudentID = student.StudentID
+        INNER JOIN course
+            ON enrollment.CourseID = course.CourseID
+        ";
+
+        return $this->conn->query($sql);
+    }
+
+    // Add Enrollment
+    public function create(
+        $studentID,
+        $courseID,
+        $date
+    ){
+
+        $sql = "
+        INSERT INTO enrollment
+        (
+            StudentID,
+            CourseID,
+            EnrollDate
+        )
+        VALUES
+        (
+            ?, ?, ?
+        )
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bind_param(
+            "iis",
+            $studentID,
+            $courseID,
+            $date
+        );
+
+        return $stmt->execute();
+    }
+
+    // Delete Enrollment
+    public function delete($id)
+    {
+        $sql = "
+        DELETE FROM enrollment
+        WHERE EnrollmentID=?
+        ";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bind_param("i", $id);
+
+        return $stmt->execute();
+    }
+}
+?>
